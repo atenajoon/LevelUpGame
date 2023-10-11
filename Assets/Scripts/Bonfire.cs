a1f11a93b5a3acfbae9ed3ba2a72a52b6ea9dfe7
Atena Razm <atenarazm@gmail.com>
2023-10-10 18:29:43 -0700

    Smoke obj on burn added to Bonfire prefab

--- Assets/Scripts/Bonfire.cs
+++ Assets/Scripts/Bonfire.cs
@@ -4,11 +4,32 @@ using UnityEngine;
 
 public class Bonfire : MonoBehaviour
 {
+    public Transform smoke;
+
+    void Start () {
+        ParticleSystem ps = smoke.GetComponent<ParticleSystem>();
+        var em = ps.emission;
+        em.enabled = false;
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
         {
+            ParticleSystem ps = smoke.GetComponent<ParticleSystem>();
+            var em = ps.emission;
+            em.enabled = true;
+            StartCoroutine (stopSmoke());
+
             other.GetComponent<PlayerController>().Burn();
         }
+    }
+    
+    IEnumerator stopSmoke()
+    {
+        yield return new WaitForSeconds(.2f);
+        ParticleSystem ps = smoke.GetComponent<ParticleSystem>();
+        var em = ps.emission;
+        em.enabled = false;
     }
 }
